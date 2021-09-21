3ed512c30a7e40c46aa310fbbd4bd84e7b3a6cbf
Matheus M. Reis <mmr94@live.com>
2021-09-20 22:27:38 -0300

    Arquivo finalizado do curso Design Patterns I

---
+++ loja/src/br/com/alura/loja/TestesPedidos.java
@@ -0,0 +1,24 @@
+package br.com.alura.loja;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+import br.com.alura.loja.pedido.GeraPedido;
+import br.com.alura.loja.pedido.GeraPedidoHandler;
+import br.com.alura.loja.pedido.acao.EnviarEmailPedido;
+import br.com.alura.loja.pedido.acao.SalvandoPedidoBD;
+
+public class TestesPedidos {
+
+	public static void main(String[] args) {
+		
+		String cliente = "Matheus";
+		BigDecimal valorOrcamento = new BigDecimal("300");
+		int quantidadeItens = Integer.parseInt("2");
+		
+		GeraPedido gerador = new GeraPedido(cliente, valorOrcamento, quantidadeItens);
+		GeraPedidoHandler handler = new GeraPedidoHandler(Arrays.asList(new SalvandoPedidoBD(), new EnviarEmailPedido()));
+		handler.executa(gerador);
+	}
+
+}
